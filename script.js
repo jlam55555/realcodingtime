d73f02f4c6a323c2db3bbe9bedf74f745bdd3a11
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 11:28:28 -0400

    Added some socket.io logic

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(() => {
-  alert("Hello, World!");
+  var msg = m => $("div#console").append("\n" + m);
+  var socket = io.connect("http://www.real-coding-time.herokapp.com");
+  socket.connect(() => {
+    msg("32");
+  });
+  socket.connect_error(() => {
+    msg("42");
+  });
+  msg("hello again?");
 });
