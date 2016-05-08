ce5cb1139713b721a2c3978c274bc978b1b0bbdf
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 13:18:29 -0400

    Moved socket event handlers inside connect handler

--- script.js
+++ script.js
@@ -8,21 +8,19 @@ $(() => {
   let msg = m => console.append("\n" + m);
   let setCode = code => codeElem.html(code.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
   
-  // event handlers
-  codeElem.on("input", () => {
-    alert(codeElem.text().replace(/\n/g, "\\n"));
-  });
-
   // socket.io
   let socket = io.connect("http://real-coding-time.herokuapp.com");
   socket.on("connect", () => {
     msg("Successfully connected to server.");
+    codeElem.on("input", () => {
+      console.log(codeElem.text().replace(/\n/g, "\\n");
+    });
+    socket.on("code", code => {
+      setCode(code);
+    });
   });
   socket.on("connect_error", () => {
     msg("Error connecting to server.");
-  });
-  socket.on("code", code => {
-    setCode(code);
-  });
+  }); 
   msg("Hello. Scripts loaded. Waiting for server...");
 });
