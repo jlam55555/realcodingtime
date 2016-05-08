730e3da39d0925bb31ef33c158e9fce5372830ad
Jonathan Lam <jlam55555@gmail.com>
2016-05-08 11:33:16 -0400

    (hopefully) added cross-domain support

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(() => {
   var msg = m => $("div#console").append("\n" + m);
-  var socket = io.connect("http://www.real-coding-time.herokapp.com");
+  var socket = io.connect("http://real-coding-time.herokapp.com:3000", {"transports": ["websocket"]});
   socket.connect(() => {
     msg("32");
   });
